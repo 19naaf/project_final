ed7e5d7b7b0198cce9580e9458ec7c65ebf696dc
Nelson Acosta <nelson@MacBook-Air-de-Nelson.local>
2024-05-07 16:08:04 +0200

    first_commit

---
+++ src/stores/task.js
@@ -0,0 +1,22 @@
+// /store/task.js
+ 
+import { defineStore } from "pinia";
+import { supabase } from "../supabase";
+ 
+export const useTaskStore = defineStore("tasks", {
+  state: () => ({
+    tasks: null,
+  }),
+  actions: {
+    async fetchTasks() {
+      const { data: tasks } = await supabase
+        .from("tasks")
+        .select("*")
+        .order("id", { ascending: false });
+      this.tasks = tasks;
+    },
+  },
+});
+
+
+//donde esta task poner profile para la otra base de datos
---
+++ src/views/Auth.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+    <h1>Componente Authentication</h1>
+    aqui va el formulario, y el formulario debe llamar a la funcion en user signup
+</template>
+
+<style scoped></style>
